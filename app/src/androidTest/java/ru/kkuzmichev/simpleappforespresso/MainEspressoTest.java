2ea115cb8cf7d7aa9b787be9b7cb3c1f40e492a3
Aleksandr Kharitonovich <harison-din@yandex.ru>
2023-09-19 23:12:49 +0300

    Initial Commit

---
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/MainEspressoTest.java
@@ -0,0 +1,35 @@
+package ru.kkuzmichev.simpleappforespresso;
+
+import androidx.test.espresso.ViewInteraction;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.espresso.action.ViewActions.click;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+
+public class MainEspressoTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+    
+    @Test
+    public void checkMainText() {
+        ViewInteraction mainText = onView(withId(R.id.text_home));
+        mainText.check(matches(isDisplayed()));
+        mainText.check(matches(withText("This is home fragment")));
+    }
+
+
+}
